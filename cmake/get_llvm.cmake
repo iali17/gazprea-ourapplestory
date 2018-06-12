c34c90e77c6d8c37ab1fc498c4e52179d0128218
Braedy Kuzma <braedy@ualberta.ca>
2018-06-12 17:17:33 -0600

    Add llvm bits to the build.

--- cmake/get_llvm.cmake
+++ cmake/get_llvm.cmake
@@ -6,9 +6,14 @@ find_package(LLVM REQUIRED CONFIG)
 message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION}")
 message(STATUS "Using LLVMConfig.cmake in: ${LLVM_DIR}")
 
-# Ensure we found our own specified version at 6.0.0. We don't want Ohaton's 3.x.x.
-if(NOT (${LLVM_VERSION_MAJOR} EQUAL 6 AND
-        ${LLVM_VERSION_MINOR} EQUAL 0 AND
-        ${LLVM_VERSION_PATCH} EQUAL 0))
+# Ensure we found our own specified version at 6.0.0. We don't want Ohaton's 3.x.x or another local
+# build we don't know.
+if(NOT ("${LLVM_VERSION_MAJOR}" EQUAL 6 AND
+        "${LLVM_VERSION_MINOR}" EQUAL 0 AND
+        "${LLVM_VERSION_PATCH}" EQUAL 0))
   message(FATAL_ERROR "LLVM version incompatible.")
 endif()
+
+# Add llvm specific pieces to our biuild.
+include_directories("${LLVM_INCLUDE_DIRS}")
+add_definitions("${LLVM_DEFINITIONS}")
