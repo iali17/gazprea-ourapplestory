f3bb6dd2bfcc3bf10ea67c45205253f9805bc1ab
Kyle LePoidevin-Gonzales <lepoidev@cs.ualberta.ca>
2018-11-04 18:12:52 -0700

    simple type added for tuples in the symbol table

---
+++ include/Scope/GazpreaTupleType.h
@@ -0,0 +1,22 @@
+//
+// Created by lepoidev on 11/4/18.
+//
+
+#ifndef GAZPREABASE_TUPLESYMBOL_H
+#define GAZPREABASE_TUPLESYMBOL_H
+#include "Symbol.h"
+#include "GazpreaType.h"
+#include <unordered_map>
+
+class GazpreaTupleType : public GazpreaType {
+protected:
+    std::unordered_map<std::string, int> *stringRefMap;
+    std::vector<llvm::Type *>            *members;
+public:
+    GazpreaTupleType(const std::string &name, llvm::Type *typeDef, std::unordered_map<std::string, int> *stringRefMap,
+                std::vector<llvm::Type *> *members);
+
+public:
+    int getOffsetFromString(std::string str);
+};
+#endif //GAZPREABASE_TUPLESYMBOL_H
