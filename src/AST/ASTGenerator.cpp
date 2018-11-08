bee7e41e5196fa3229a5f4ddca21ddc95e92ea90
Imran Ali <iali1@cs.ualberta.ca>
2018-11-08 13:40:08 -0700

    pulling changes

--- src/AST/ASTGenerator.cpp
+++ src/AST/ASTGenerator.cpp
@@ -386,7 +386,6 @@ antlrcpp::Any ASTGenerator::visitNormalDecl(gazprea::GazpreaParser::NormalDeclCo
 
     // if decl is a tuple decl then
     if ((ctx->type().size() == 1) && (ty.substr(0, 5) == "tuple")) {
-
         return (ASTNode *) new TupleDeclNode(expr, constant, id, visit(ctx->type(0)));
 
     } else { // else it's a normal decl
